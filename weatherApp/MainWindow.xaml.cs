58c6657c1d01e84dfc8726835d037a233782d1d5
Utku <utku.canal@avkaryazilim.com.tr>
2022-01-10 01:42:52 +0300

    WeatherApp %85

--- weatherApp/MainWindow.xaml.cs
+++ weatherApp/MainWindow.xaml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Timers;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -20,9 +21,92 @@ namespace weatherApp
     /// </summary>
     public partial class MainWindow : Window
     {
+        System.Timers.Timer t;
+        int sec;
+        String city="Ankara";
+        weatherApi weatherApi = new weatherApi();
         public MainWindow()
         {
             InitializeComponent();
+            timer();
+            getWeather();
+
         }
+
+        private void OnTimeEvent(object? sender, ElapsedEventArgs e)
+        {
+            sec += 1;
+            
+            if(sec == 5)
+            {
+                getWeather();
+                sec = 0;
+                if (weatherApi.Description.Equals("Kar Yağışlı"))
+                {
+                    img_temp.Dispatcher.Invoke(new Action(() =>
+                    {
+                        img_temp.Visibility = Visibility.Visible;
+                    }));
+                    
+                }
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+        private void getWeather()
+        {
+            try
+            {
+                
+                weatherApi.getApi(city);
+                lbl_name.Dispatcher.Invoke(new Action(() =>
+                {
+                    lbl_name.Content = weatherApi.Name;
+                }));
+
+                lbl_temp.Dispatcher.Invoke(new Action(() =>
+                {
+                    lbl_temp.Content = weatherApi.Temp + " °C";
+                }));
+
+                lbl_description.Dispatcher.Invoke(new Action(() =>
+                {
+                    lbl_description.Content = weatherApi.Description;
+                }));
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message);
+            }
+               
+        }
+        private void OnKeyDownHandler(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Return)
+            {
+                txt_city.Text=txt_city.Text.Trim();
+                city=txt_city.Text;
+                getWeather();
+            }
+        }
+        private void timer()
+        {
+            t = new System.Timers.Timer();
+            t.Interval = 1000;
+            t.Elapsed += OnTimeEvent;
+            t.Start();
+        }
+
+        private void Window_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.LeftButton==MouseButtonState.Pressed)
+            {
+                this.DragMove();
+            }
+        }
+
     }
 }
---
+++ weatherApp/weatherApi.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace weatherApp
+{
+    internal class weatherApi
+    {
+        private static String api = "92b87b907bc245d768b29aa895323423";
+
+        private String name,temp,description;
+        public String Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        public String Temp
+        {
+            get { return temp; }
+            set { temp = value; }
+        }
+        public String Description
+        {
+            get { return description; }
+            set { description = value; }
+        }
+
+        public weatherApi()
+        {
+
+        }
+        public void getApi(String city)
+        {
+            String conApi = "https://api.openweathermap.org/data/2.5/weather?q=" + city +"&mode=xml&appid=" + api + "&units=metric&lang=tr";
+            XDocument weather=XDocument.Load(conApi);
+
+             Name=weather.Descendants("city").ElementAt(0).Attribute("name").Value;
+             Temp = weather.Descendants("temperature").ElementAt(0).Attribute("value").Value;
+             Description = weather.Descendants("weather").ElementAt(0).Attribute("value").Value;
+        }
+
+    }
+}
